78332e51775d9c5c39f23d7de66791cec996fcbc
Mujavid Bukhari <sirmuj@gmail.com>
2017-01-31 17:04:19 +0000

    Move files out of meeting-app

--- src/app/all-meetings.component.ts
+++ src/app/all-meetings.component.ts
@@ -12,7 +12,15 @@ export class AllMeetingsComponent {
 
 	ngOnInit() {
 		this.meetings = [
-							{"id":"1", "name":"Plan Client Presentation", "time":"Tuesday 2nd February at 14:30", "description":"Example description", "location":"MPEB 6.21, UCL", "minutes":"This is what we talked about", "agenda":"This is what we talked about"}
+							{
+								"id"			: "1", 
+								"name"			: "Plan Client Presentation", 
+								"time"			: "Tuesday 2nd February at 14:30", 
+								"description"	: "Example description", 
+								"location"		: "MPEB 6.21, UCL", 
+								"minutes"		: "This is what we talked about", 
+								"agenda"		: "This is what we talked about"
+							}
 						];
 	}
 }
