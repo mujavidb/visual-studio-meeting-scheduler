694c095f417eac91baea04db38fe4c320e4d43b8
Alasdair Hall <Alasdair Hall>
2017-02-02 16:29:10 +0000

    chnges to component

--- meeting-app/src/app/app.component.ts
+++ meeting-app/src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'app-root',
-  	template: '<router-outlet></router-outlet>',
-  	styleUrls: ['./app.component.css']
+  	template: '<router-outlet></router-outlet>'
 })
 
 export class AppComponent {
