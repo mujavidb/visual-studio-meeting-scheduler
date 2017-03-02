1c73052f8d67cab94ea6e5b99a050393fe8aedcd
Alasdair Hall <Alasdair Hall>
2017-03-02 16:07:30 +0000

    Multiple full calendar events can now be created

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -8,7 +8,42 @@ export default class Calendar extends Component {
 	componentDidMount() {
 		const { calendar } = this.refs;
 
-		$(calendar).fullCalendar(calendarConfig);
+		$(calendar).fullCalendar({
+		    header: {
+		    	left: '',
+				center: 'title',
+				right: ''
+		    },
+		    titleFormat: 'MMM D YYYY',
+		    defaultView: 'agendaWeek',
+		    defaultDate: moment(),
+		    firstDay: 1,
+		    scrollTime: '08:00:00',
+		    columnFormat: 'ddd D/M',
+		    allDaySlot: false,
+		    navLinks: false, // can click day/week names to navigate views
+			selectable: true,
+			selectHelper: true,
+			select: function(start, end) {
+				var title = "Meeting Slot";
+				var eventData = {
+					title: title,
+					start: start,
+					end: end
+				};
+				$(calendar).fullCalendar('renderEvent', eventData, true); // stick? = true
+				$(calendar).fullCalendar('unselect');
+			},
+			eventClick: function(calEvent, jsEvent, view) {
+				view = $(calendar).fullCalendar('getView');
+				console.log(view);
+				$(calendar).fullCalendar('removeEvents', calEvent._id);
+			},
+			editable: true,
+			eventOverlap: true,
+			eventLimit: true, // allow "more" link when too many events
+
+		});
 	}
 
 	render() {
