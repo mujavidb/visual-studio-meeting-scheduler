a5e691e75f8af2e95e29f2b2928620c3b1f5f94e
2khc <kh.kelvinchan@gmail.com>
2017-03-03 23:16:47 +0000

    generateMeetingId():
    Uses hashid package and a meeting number count to generate
    a unique ID for each meeting.

--- documentdb/server/Classes/Meeting.js
+++ documentdb/server/Classes/Meeting.js
@@ -1,13 +1,26 @@
+var Hashids = require('hashids');
+
+var Collection = require('../functions/collections');
+var config = require('../config');
+
+var documentClient = require('documentdb').DocumentClient;
+var client = new documentClient(config.endpoint, {"masterKey": config.primaryKey});
+
+var databaseUrl = `dbs/${config.database.id}`;
+var collectionUrl = `${databaseUrl}/colls/${config.collection.id}`;
+var meetingsDocumentUrl = `${collectionUrl}/docs/meetings`;
+var salt = 'nXqPlBBYVdP5Qhxj39PE';
+
 class Meeting {
 
-    constructor(hostId, meetingId, meetingName) {
-        this.hostId = hostId;
-        this.meetingId = meetingId;
-        this.meetingName = meetingName;
+    constructor(hostId, meetingName) {
+        // this.hostId = hostId; this.meetingId = this.generateMeetingId();
+        // this.meetingName = meetingName;
+
 
         this.data = {
             "hostId": hostId,
-            "meetingId": meetingId,
+            "meetingId": null,
             "meetingName": meetingName,
             "hostAvailability": [],
             "finalDate": null,
@@ -15,22 +28,62 @@ class Meeting {
         }
     }
 
-    addHostAvailability(dateStart, dateEnd) {
-        this.data.hostAvailability.push({
-            "dateStart": dateStart,
-            "dateEnd": dateEnd
+    generateMeetingId() {
+
+        // We need to generate unique IDs for our meeting. What we'll do is have a
+        // seperate collection to store the number of meetings created. We'll then use
+        // this number with hashIds package to generate an ID.
+        var self = this;
+        var id = "";
+        return new Promise((resolve, reject) => {
+
+            // First we get the document.
+            client.readDocument(meetingsDocumentUrl, (error, result) => {
+                if (error) {
+                    console.log("Error in reading meetingsDocument");
+                    reject(error);
+                } else {
+                    // Now we generate the id.
+                    var hashNumber = result.count + 1;
+                    var hashids = new Hashids(salt);
+                    var int1 = Math.floor(Math.random * 1000);
+                    var int2 = Math.floor(Math.random * 1000);
+
+                    id = hashids.encode(12387, 823754922, hashNumber);
+
+                    console.log("Your hash is: " + id);
+
+                    // Now we have the id, we need to update the document.
+                    result.count = hashNumber;
+
+                    client.replaceDocument(meetingsDocumentUrl, result, (error, result) => {
+                        if (error) {
+                            console.log("Writing hashID failed.");
+                        } else {
+                            console.log("Writing hashID succeeded.");
+                        }
+                    });
+                    console.log("self data: " + self.data);
+                    self.data.meetingId = id;
+                    resolve(id);
+                }
+            })
         });
+        
+    }
+
+    addHostAvailability(dateStart, dateEnd) {
+        this
+            .data
+            .hostAvailability
+            .push({"dateStart": dateStart, "dateEnd": dateEnd});
     }
 
     addAttendee(id, name) {
-        this.data.attendees.push(
-            {
-                "id": id,
-                "response": 0,
-                "name": name,
-                "availableTimes": []
-            }
-        );
+        this
+            .data
+            .attendees
+            .push({"id": id, "response": 0, "name": name, "availableTimes": []});
     }
 
 }
