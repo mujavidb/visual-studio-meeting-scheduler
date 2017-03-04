d48e2c0c6535905f0c3d136b3b1db9d9dd9bc2c5
2khc <kh.kelvinchan@gmail.com>
2017-03-04 13:47:22 +0000

    Removed unecessary code

--- documentdb/server/functions/collections.js
+++ documentdb/server/functions/collections.js
@@ -49,79 +49,8 @@ exports.createDocument = function (accountId) {
 
     var data = {
         "id": accountId,
-        "meetings": [{
-            "hostId": "1234",
-            "meetingId": "4321",
-            "meetingName": "Birthday!",
-            "hostAvailability": [{
-                "dateStart": "01/12/2017",
-                "dateEnd": "00:00"
-            }, {
-                "dateStart": "01/12/2017",
-                "dateEnd": "00:00"
-            }],
-            "finalDate": "some date object here",
-            "attendees": [{
-                "id": "asqwe12d",
-                "response": 1,
-                "name": "Kelvin",
-                "availableTimes": [{
-                    "dateStart": "01/12/2017",
-                    "dateEnd": "00:00"
-                }, {
-                    "dateStart": "01/12/2017",
-                    "dateEnd": "00:00"
-                }]
-            }, {
-                "id": "asqwe12d",
-                "response": 1,
-                "name": "Kelvin",
-                "availableTimes": [{
-                    "dateStart": "01/12/2017",
-                    "dateEnd": "00:00"
-                }, {
-                    "dateStart": "01/12/2017",
-                    "dateEnd": "00:00"
-                }]
-            }]
-        }, {
-            "hostId": "4321",
-            "meetingId": "4321",
-            "meetingName": "Birthday!",
-            "hostAvailability": [{
-                "dateStart": "01/12/2017",
-                "dateEnd": "00:00"
-            }, {
-                "dateStart": "01/12/2017",
-                "dateEnd": "00:00"
-            }],
-            "finalDate": "some date object here",
-            "attendees": [{
-                "id": "asqwe12d",
-                "response": 1,
-                "name": "Kelvin",
-                "availableTimes": [{
-                    "dateStart": "01/12/2017",
-                    "dateEnd": "00:00"
-                }, {
-                    "dateStart": "01/12/2017",
-                    "dateEnd": "00:00"
-                }]
-            }, {
-                "id": "asqwe12d",
-                "response": 1,
-                "name": "Kelvin",
-                "availableTimes": [{
-                    "dateStart": "01/12/2017",
-                    "dateEnd": "00:00"
-                }, {
-                    "dateStart": "01/12/2017",
-                    "dateEnd": "00:00"
-                }]
-            }]
-        }]
-
-    }
+        "meetings": []
+    };
 
     return new Promise((resolve, reject) => {
         client.readDocument(documentUrl, (error, result) => {
