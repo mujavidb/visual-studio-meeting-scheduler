ce184917fcd533f365d94e102d87dbc95c291834
2khc <kh.kelvinchan@gmail.com>
2017-03-07 13:31:22 +0000

    removed 'collection' path in URL

--- src/server/app.js
+++ src/server/app.js
@@ -27,9 +27,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/', collection);
 app.use('/users', users);
-app.use('/collection', collection);
+// app.use('/collection', collection);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
